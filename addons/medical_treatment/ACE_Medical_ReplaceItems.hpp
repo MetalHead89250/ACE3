8fb40f07f77b67abc7324887b4027217f5510ff9
SilentSpike <SilentSpike100+Github@gmail.com>
2019-05-22 17:29:26 +0100

    Add the replace items config file
    Forgot to stage it in my initial commit

---
+++ addons/medical_treatment/ACE_Medical_ReplaceItems.hpp
@@ -0,0 +1,17 @@
+class GVAR(replaceItems) {
+    FirstAidKit[] = {
+        {"ACE_fieldDressing", 1},
+        {"ACE_packingBandage", 1},
+        {"ACE_morphine", 1},
+        {"ACE_tourniquet", 1}
+    };
+    Medikit[] = {
+        {"ACE_packingBandage", 2},
+        {"ACE_fieldDressing", 1},
+        {"ACE_epinephrine", 1},
+        {"ACE_morphine", 1},
+        {"ACE_salineIV_250", 1},
+        {"ACE_tourniquet", 1}
+    };
+};
+
